7baeab9e88b00c5d133bf8c9e3f42e1dc416d537
chungmingyuan <chungmingyuan@users.noreply.github.com>
2017-08-09 15:45:11 -0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php 
 $DB_HOST = 'localhost';
 $DB_USER = 'root';
-$DB_PASS = 'pineapple1221';
+$DB_PASS = 'pineapplexxxx';
 $DB_NAME = 'tubbi';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 $mysqli->set_charset("utf8"); // for chinese characters
